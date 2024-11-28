511e32fb20c1280d8a5e8d89e202e5a0f1a86d3f
kiran <kirangawande9307@gmail.com>
2024-11-28 16:20:00 +0530

    upload all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { useState } from 'react';
+import 'react-notifications/lib/notifications.css';
+import { NotificationContainer,NotificationManager } from 'react-notifications';
+import { upper,lower,num,sym } from './Data';
 
-function App() {
+export default function App() {
+  let [passwordlen, setPasswordlen] = useState(10)
+  let [uppercase, setUppercase] = useState(false)
+  let [lowercase, setLowercase] = useState(false)
+  let [number, setNumber] = useState(false)
+  let [symbols, setSymbols] = useState(false)
+  let [fpass, setFpass] = useState('')
+
+  let passgenrate = () => {
+    let setchar = ''
+    let finalpass = ''
+    if (uppercase || lowercase || number || symbols) {
+      if (uppercase) setchar += upper;
+      if (lowercase) setchar += lower;
+      if (number) setchar += num;
+      if (symbols) setchar += sym;
+      for (let i = 0; i <= passwordlen-1; i++) {
+        finalpass += setchar.charAt(Math.floor(Math.random() * setchar.length))
+      }
+      setFpass(finalpass)
+      NotificationManager.success("Password Generated Sucessfully....");
+
+    }
+    else {
+      NotificationManager.error("Please Select Any 1 CheckBox");
+    }
+  }
+
+  let copyText = () => {
+    navigator.clipboard.writeText(fpass)
+    if(fpass.length>=1){
+      NotificationManager.success("Password Copy Sucessfully....");
+      setFpass('')
+
+    }
+    else{
+      NotificationManager.error("Password Not Avaliable");
+    }
+    
+
+  }
+  let clear=()=>{
+    setFpass('')
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <NotificationContainer/>
+      <div class="container">
+        <h1>Password Generator</h1>
+        <form id="passwordForm">
+          <div class="form-group">
+            <label for="passwordLength">Password Length:</label>
+            <input type="number" id="passwordLength" min={10} max={20} value={passwordlen} onChange={(e) => setPasswordlen(e.target.value)} />
+          </div>
+          <div class="form-group">
+            <label for="uppercase">Include Uppercase:</label>
+            <input type="checkbox" id="uppercase" checked={uppercase} onChange={(e) => setUppercase(!uppercase)} />
+          </div>
+          <div class="form-group">
+            <label for="lowercase">Include Lowercase:</label>
+            <input type="checkbox" id="lowercase" checked={lowercase} onChange={(e) => setLowercase(!lowercase)} />
+          </div>
+          <div class="form-group">
+            <label for="numbers">Include Numbers:</label>
+            <input type="checkbox" id="numbers" checked={number} onChange={(e) => setNumber(!number)} />
+          </div>
+          <div class="form-group">
+            <label for="symbols">Include Symbols:</label>
+            <input type="checkbox" id="symbols" checked={symbols} onChange={(e) => setSymbols(!symbols)} />
+          </div>
+          <button type="button" onClick={passgenrate} id="generateBtn">Generate Password</button>
+          <div class="form-group">
+            <label for="generatedPassword">Generated Password:</label>
+            <input type="text" value={fpass} id="generatedPassword" />
+            <button onClick={clear}>Clear</button>
+            <button type="button" id="copyBtn" onClick={copyText}>Copy</button>
+          </div>
+        </form>
+      </div>
+    </>
+  )
 }
-
-export default App;
---
+++ src/Data.js
@@ -0,0 +1,4 @@
+export let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+export let lower = "abcdefghijklmnopqrstuvwxyz"
+export let num = "01345789"
+export let sym = "@#*\/&()"
